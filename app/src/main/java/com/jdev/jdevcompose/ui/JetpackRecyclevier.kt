8d33bdd923350a4a29e19f5dec9951c598f4d89f
jdev <juanj@jdev.com>
2024-09-05 16:09:26 -0600

    add scroll coroutines

--- app/src/main/java/com/jdev/jdevcompose/ui/JetpackRecyclevier.kt
+++ app/src/main/java/com/jdev/jdevcompose/ui/JetpackRecyclevier.kt
@@ -7,15 +7,26 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,6 +37,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.jdev.jdevcompose.R
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -53,9 +65,63 @@ fun SimpleRecycleView() {
 @Composable
 fun SuperHeroView() {
     val context = LocalContext.current
+    val rvState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
 
-    LazyRow(
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        LazyColumn(
+            verticalArrangement =  Arrangement.spacedBy(8.dp),
+            state = rvState,
+            modifier = Modifier.weight(1f)
+        ) {
+            items(getSuperheroes()) { hero ->
+                ItemHero(
+                    superhero = hero,
+                    onItemSelected = {
+                        Toast.makeText(context, it.superheroName, Toast.LENGTH_SHORT).show()
+                    }
+                )
+            }
+        }
+
+        val showButton by remember {
+            derivedStateOf {
+                rvState.firstVisibleItemIndex > 0
+            }
+        }
+
+        if(showButton) {
+            Button(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp),
+                onClick = {
+
+                    coroutineScope.launch {
+                        rvState.animateScrollToItem(0)
+                    }
+
+                }
+            ) {
+                Text(text = "Button")
+            }
+        }
+
+    }
+}
+
+@Preview(
+    showSystemUi = true
+)
+@Composable
+fun SuperHeroGridView() {
+    val context = LocalContext.current
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
     ) {
         items(getSuperheroes()) { hero ->
             ItemHero(
@@ -76,7 +142,7 @@ fun ItemHero(
     Card(
         border = BorderStroke(2.dp, Color.Red),
         modifier = Modifier
-            .width(200.dp)
+            //.width(200.dp)
             .clickable {
                 onItemSelected(superhero)
             }
@@ -101,7 +167,8 @@ fun ItemHero(
             Text(
                 text = superhero.publisher,
                 fontSize = 10.sp,
-                modifier = Modifier.align(Alignment.End)
+                modifier = Modifier
+                    .align(Alignment.End)
                     .padding(8.dp)
             )
         }
